802deec292a36a9e342ab782031fa2556e646351
HenryCapdevilla <henrydaniel2002@hotmail.com>
2025-03-31 23:41:43 -0500

    Add allowedHosts apoyovivo.ddns.net

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [ tailwindcss()],
+  plugins: [tailwindcss()],
   server: {
     port: 3000,
-  }
+    allowedHosts: [
+      'apoyovivo.ddns.net', // Agrega tu dominio aquí
+    ],
+  },
 });
